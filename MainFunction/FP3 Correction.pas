c079b68d83311734affe9a74ad4760faefa09c16
H2WO4 <arthurlamidel@gmail.com>
2021-11-14 15:10:23 +0100

    Optimisation mineur de la solutionCorrection anotée de

---
+++ MainFunction/FP3 Correction.pas
@@ -0,0 +1,41 @@
+program DigitArrToBinArr;
+
+// Trois types d'erreurs possibles :
+// F -> Forme : Pas nécessaire à la machine, mais meilleur à la lecture
+// S -> Syntaxe : Erreur pour la machine
+// L -> Logique : Ca marche, mais pas comme voulu
+
+// Les corrections sont indiquées sous la forme :
+// C: Original -> Corigée
+// C: Description de l'ajout
+
+
+
+// F: Case soit minuscule, soit majuscule pour les identifiants
+type // C: Type -> type
+// F: On indente pour bien séparer les types
+// C: Identation des lignes
+    arrayInt = array of integer;
+
+    // F: Ces deux noms ne décrivent pas bien ce que représente chaque type
+    // C: tableau, ArrayTableau -> Nibble, ArrayNibble (Un nibble est 4 bits) (Un nom comme reprBin aurait aussi convenu)
+    nibble = array [1..4] of boolean;
+    arrayNibble = array of nibble;
+
+// F: Un retour à la ligne + indentation et meilleur, surtout avec un grand nombre de variables
+// C: Ajout d'une indentation
+var
+    i, t : integer;
+
+procedure DigitArrToBinArr(var digitArr: arrayInt; var binArr: arrayNibble);
+begin
+    t := taille(digitArr);
+    FixeTaille(binArr,t)
+    // S: La variable n'est pas initialisée
+    // F: La taille du tableau est calculée à nouveau, alors qu'elle est déjà dans t
+    // C: i := -> i := 0 - taille(digitArr) -> t
+    for i := 0 to t - 1 do
+    begin
+        ConvBin(digitArr[i], binArr[i])
+    end;
+end;
